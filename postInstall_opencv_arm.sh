8b48781019970d7be6c9e011d09fbb976e40e276
step66 <red.mailhub@gmail.com>
2017-04-07 03:19:43 +0800

    Update postInstall_opencv_arm.sh
    out of resources. reduced cmake params

--- postInstall_opencv_arm.sh
+++ postInstall_opencv_arm.sh
@@ -35,13 +35,9 @@ cd ~/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-  -D CMAKE_INSTALL_PREFIX=/usr/local \
-  -D INSTALL_C_EXAMPLES=ON \
-  -D INSTALL_PYTHON_EXAMPLES=ON \
-  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-  -D BUILD_EXAMPLES=ON ..
+  -D CMAKE_INSTALL_PREFIX=/usr/local ..
 
-make -j4
+make 
 
 sudo make install
 sudo ldconfig
