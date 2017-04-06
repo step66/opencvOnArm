26b47b946d237af23991a995de475d687ef2bb6c
step66 <red.mailhub@gmail.com>
2017-04-07 02:35:19 +0800

    try using 3.0.0

--- postInstall_opencv_arm.sh
+++ postInstall_opencv_arm.sh
@@ -1,46 +1,46 @@
 # This script installs opencv 3.1.0 onto an x64 device
-# Taken from http://www.pyimagesearch.com/2016/04/18/install-guide-raspberry-pi-3-raspbian-jessie-opencv-3/
-# Error during make: protobuf unable to be used
+# Taken from http://www.pyimagesearch.com/2015/06/22/install-opencv-3-0-and-python-2-7-on-ubuntu/
 # Change permissions first with: chmod 0755 opencv-on-x64.sh
 # Then run with: ./opencv-on-x64.sh
 
-sudo apt-get install build-essential cmake pkg-config libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libxvidcore-dev libx264-dev libgtk2.0-dev libatlas-base-dev gfortran python2.7-dev python3-dev
+sudo apt-get install build-essential cmake git pkg-config libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran
 
-cd ~
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.1.0.zip
-unzip opencv.zip
+#Set up pip and venv
+#wget https://bootstrap.pypa.io/get-pip.py
+#sudo python get-pip.py
+#sudo pip install virtualenv virtualenvwrapper
+#sudo rm -rf ~/.cache/pip
 
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.1.0.zip
-unzip opencv_contrib.zip
-
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-
-sudo pip install virtualenv virtualenvwrapper
-sudo rm -rf ~/.cache/pip
-
-echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.profile
-echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.profile
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.profile
-
-source ~/.profile
-
-mkvirtualenv cv -p python2
-
+#echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
+#echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+#source ~/.bashrc
+#mkvirtualenv cv 
 workon cv
 
-pip install numpy
+#pip install numpy
 
-cd ~/opencv-3.1.0/
+cd ~
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+git checkout 3.0.0
+
+cd ~
+git clone https://github.com/Itseez/opencv_contrib.git
+cd opencv_contrib
+git checkout 3.0.0
+
+cd ~/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-3.1.0/modules \
-    -D BUILD_EXAMPLES=ON ..
+  -D CMAKE_INSTALL_PREFIX=/usr/local \
+  -D INSTALL_C_EXAMPLES=ON \
+  -D INSTALL_PYTHON_EXAMPLES=ON \
+  -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+  -D BUILD_EXAMPLES=ON ..
 
-make
+make -j4
 
 sudo make install
 sudo ldconfig
@@ -50,6 +50,5 @@ ls -l /usr/local/lib/python2.7/site-packages/
 cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
 ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
 
-source ~/.profile 
 workon cv
 python
