f459313d1b96496896ab3a951582ee9d42030d66
step66 <red.mailhub@gmail.com>
2017-05-10 10:56:57 +0800

    Update postInstall_opencv_arm.sh

--- postInstall_opencv_arm.sh
+++ postInstall_opencv_arm.sh
@@ -9,45 +9,45 @@ sudo apt-get upgrade -y
 sudo apt-get install build-essential cmake git pkg-config libjpeg8-dev libtiff5-dev libjasper-dev libpng12-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran -y
 
 #Set up pip and venv
-#wget https://bootstrap.pypa.io/get-pip.py
-#sudo python get-pip.py
-#sudo pip install virtualenv virtualenvwrapper
-#sudo rm -rf ~/.cache/pip
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+sudo pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/.cache/pip
 
-#echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
-#echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
-#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-#source ~/.bashrc
-#mkvirtualenv cv 
-#workon cv
-
-#pip install numpy
-
-cd ~
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-git checkout 3.0.0
-
-cd ~
-git clone https://github.com/Itseez/opencv_contrib.git
-cd opencv_contrib
-git checkout 3.0.0
-
-cd ~/opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-  -D CMAKE_INSTALL_PREFIX=/usr/local ..
-
-make 
-
-sudo make install
-sudo ldconfig
-#stuck
-ls -l /usr/local/lib/python2.7/site-packages/
-
-cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
-ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
-
+echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
+echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc
+echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv cv 
 workon cv
-python
+
+pip install numpy
+
+#cd ~
+#git clone https://github.com/Itseez/opencv.git
+#cd opencv
+#git checkout 3.0.0
+
+#cd ~
+#git clone https://github.com/Itseez/opencv_contrib.git
+#cd opencv_contrib
+#git checkout 3.0.0
+
+#cd ~/opencv
+#mkdir build
+#cd build
+#cmake -D CMAKE_BUILD_TYPE=RELEASE \
+#  -D CMAKE_INSTALL_PREFIX=/usr/local ..
+
+#make 
+
+#sudo make install
+#sudo ldconfig
+#stuck
+#ls -l /usr/local/lib/python2.7/site-packages/
+
+#cd ~/.virtualenvs/cv/lib/python2.7/site-packages/
+#ln -s /usr/local/lib/python2.7/site-packages/cv2.so cv2.so
+
+#workon cv
+#python
